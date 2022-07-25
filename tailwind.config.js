5b61b834b5563c840663a2400a49909379f7417a
vaaakoo <vaaakoo@gmail.com>
2022-07-26 03:02:36 +0400

    second changes for my repo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,17 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 
 module.exports = {
+  darkMode: 'class',
+  content: ["./*.html"],
   theme: {
     screens: {
       'xs': '475px',
       ...defaultTheme.screens,
 
       'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'mm': '840px',
       // => @media (min-width: 640px) { ... }
 
       'md': '1024px',
@@ -19,7 +24,11 @@ module.exports = {
     extend: {
       // ...
       colors: {
-
+        'light-grey': '#e5e7eb',
+        'midnight': '#4b5563',
+        'orange-scoot': '#E99210',
+        'b-red': '#E54D25',
+        'vector-collor': '#34353F',
       },
     },
   },
